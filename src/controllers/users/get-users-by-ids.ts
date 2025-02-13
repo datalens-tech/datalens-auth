e28282d76a003af15c61f0e4acb86f1e8c5ec7ec
Evegniy Shangin <shangin@yandex-team.ru>
2025-02-13 15:34:56 +0300

    Users by ids

---
+++ src/controllers/users/get-users-by-ids.ts
@@ -0,0 +1,55 @@
+import {AppRouteHandler, Response} from '@gravity-ui/expresskit';
+
+import {ApiTag} from '../../components/api-docs';
+import {makeReqParser, z, zc} from '../../components/zod';
+import {CONTENT_TYPE_JSON} from '../../constants/content-type';
+import {getUsersByIds} from '../../services/users/get-users-by-Ids';
+
+import {UsersByIdsResponseModel, usersByIdsModel} from './response-models/users-model';
+
+const requestSchema = {
+    body: z.object({
+        subjectIds: zc.decodeIdArray({min: 1, max: 1000}),
+    }),
+};
+
+const parseReq = makeReqParser(requestSchema);
+
+const controller: AppRouteHandler = async (req, res: Response<UsersByIdsResponseModel>) => {
+    const {body} = await parseReq(req);
+
+    const result = await getUsersByIds(
+        {ctx: req.ctx},
+        {
+            subjectIds: body.subjectIds,
+        },
+    );
+
+    res.status(200).send(await usersByIdsModel.format(result));
+};
+
+controller.api = {
+    summary: 'Users list by ids',
+    tags: [ApiTag.Users],
+    request: {
+        body: {
+            content: {
+                [CONTENT_TYPE_JSON]: {
+                    schema: requestSchema.body,
+                },
+            },
+        },
+    },
+    responses: {
+        200: {
+            description: usersByIdsModel.schema.description ?? '',
+            content: {
+                [CONTENT_TYPE_JSON]: {
+                    schema: usersByIdsModel.schema,
+                },
+            },
+        },
+    },
+};
+
+export {controller as getUsersByIds};
--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -1,4 +1,5 @@
 import {getUserProfile} from './get-user-profile';
+import {getUsersByIds} from './get-users-by-ids';
 import {getUsersList} from './get-users-list';
 import {updateUserPassword} from './update-user-password';
 import {updateUserProfile} from './update-user-profile';
@@ -6,6 +7,7 @@ import {updateUserProfile} from './update-user-profile';
 export default {
     getUsersList,
     getUserProfile,
+    getUsersByIds,
     updateUserPassword,
     updateUserProfile,
 };
