3ee23fa76b1f0c7bc21c12a69161b1545e93e3fc
Georgy Malkov <malkovgv@yandex-team.ru>
2025-04-10 13:57:14 +0300

    Up ubuntu version to 24 and Node.js to 22 (#51)

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# DataLens Auth Service
-🐶
+# Auth Service (Auth)
+
+Auth Service is part of [DataLens](https://datalens.tech) that provides base authentification and authorization layer.
+
+## Getting started
+
+```sh
+npm ci
+npm run dev
+```
+
+[More details](https://github.com/datalens-tech/datalens)
--- scripts/preflight.sh
+++ scripts/preflight.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -e
 
-if [ "$SKIP_INSTALL_DB_EXTENSIONS" = "1" ]; then
-    echo 'Skip extensions setting up'
+if [ "${SKIP_INSTALL_DB_EXTENSIONS}" = "1" ]; then
+    echo '{"level":"INFO","msg":"Skip extensions setting up"}'
 else
-    echo "Start setting up extensions"
+    echo '{"level":"INFO","msg":"Start setting up extensions"}'
     node /opt/app/dist/server/db/scripts/preflight/extensions.js
-    echo "Finish setting up extensions"
+    echo '{"level":"INFO","msg":"Finish setting up extensions"}'
 fi
 
-echo "Start migration"
+echo '{"level":"INFO","msg":"Start migration"}'
 npm run db:migrate
-echo "Finish migration"
+echo '{"level":"INFO","msg":"Finish migration"}'
 
-echo "Start creating default user Admin"
+echo '{"level":"INFO","msg":"Start creating default user Admin"}'
 node /opt/app/dist/server/db/scripts/preflight/create-default-admin.js
-echo "Finish creating default user Admin"
+echo '{"level":"INFO","msg":"Finish creating default user Admin"}'
 
-node dist/server
+exec 'node' 'dist/server'
